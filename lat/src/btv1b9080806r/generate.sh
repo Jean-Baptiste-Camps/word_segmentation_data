0ab4e45a6fadb81cc9726a48078f5c6c011a13f8
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-05-12 19:59:12 +0200

    revision of training sets

--- lat/src/btv1b9080806r/generate.sh
+++ lat/src/btv1b9080806r/generate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-boudams dataset convert plain-text ./gt txt/*.txt --max_char_length 150 --random_keep 0 --noise_char_random 0
-boudams dataset generate ../../tsv/oriflamms gt/*.txt  --max_char_length 150
-
+#boudams dataset convert plain-text ./gt *.txt --max_char_length 150 --random_keep 0 --noise_char_random 0
+#For now, with dev boudams: 
+#convert("datasets/btvbetc/*.txt", "datasets/btvbetc/out", max_char_length=150, random_keep=0, noise_char_random=0)
+cp gt/train_DixFolios.txt ../../tsv/btv1b9080806r_abbr/train.tsv
+cp gt/dev_DixFolios.txt ../../tsv/btv1b9080806r_abbr/dev.tsv
+cp gt/test_Folio11.txt ../../tsv/btv1b9080806r_abbr/test.tsv
