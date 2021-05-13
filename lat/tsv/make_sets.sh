0f87e1175ca708469b2b4831be989ebed5656563
Jean Batiste Camps <jbcamps@srv-rech01.enc.lan>
2021-05-13 09:05:29 +0200

    better sets

--- lat/tsv/make_sets.sh
+++ lat/tsv/make_sets.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # btv and Oriflamms
-cat oriflamms/* btv1b9080806r_paper/train.tsv >> exper2_train.tsv
+rm exper2_train.tsv
+rm exper3_train.tsv
+cat oriflamms/* btv1b9080806r_abbr/train.tsv >> exper2_train.tsv
 cat exper2_train.tsv PL/* >> exper3_train.tsv
 #cat */train.tsv */unknown.tsv > train.tsv
 #cat */dev.tsv > dev.tsv
