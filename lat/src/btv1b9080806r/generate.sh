27d8f0bf9b6852f86e0a861085f69550c589a950
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-05-14 12:33:28 +0200

    forgot to shuffle

--- lat/src/btv1b9080806r/generate.sh
+++ lat/src/btv1b9080806r/generate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#boudams dataset convert plain-text ./gt *.txt --max_char_length 150 --random_keep 0 --noise_char_random 0
+#############
 #For now, with dev boudams: 
 #convert("datasets/btvbetc/*.txt", "datasets/btvbetc/out", max_char_length=150, random_keep=0, noise_char_random=0)
-cp gt/train_DixFolios.txt ../../tsv/btv1b9080806r_abbr/train.tsv
-cp gt/dev_DixFolios.txt ../../tsv/btv1b9080806r_abbr/dev.tsv
-cp gt/test_Folio11.txt ../../tsv/btv1b9080806r_abbr/test.tsv
+# generate("datasets/btvbetc/split", "datasets/btvbetc/out/train_DixFolios.txt", 150, 1, 0)
+# from boudams.dataset.base
+# split("datasets/btvbetc/out/train_DixFolios.txt", "datasets/btvbetc/split", (1, 0,0), 150)
+# split("datasets/btvbetc/out/dev_DixFolios.txt", "datasets/btvbetc/split", (0, 1,0), 150)
+# split("datasets/btvbetc/out/test_Folio11.txt", "datasets/btvbetc/split", (0, 0, 1), 150)
+# check("./datasets/btvbetc/split", (1,1,1), 150)
+#############
+#cp gt/train_DixFolios.txt ../../tsv/btv1b9080806r_abbr/train.tsv
+#cp gt/dev_DixFolios.txt ../../tsv/btv1b9080806r_abbr/dev.tsv
+#cp gt/test_Folio11.txt ../../tsv/btv1b9080806r_abbr/test.tsv
--- lat/src/oriflamms_noallogr/generate.sh
+++ lat/src/oriflamms_noallogr/generate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#############
 #for now, use command in boudams dev ;)
-#boudams dataset convert plain-text ./gt txt/*.txt --max_char_length 150 --random_keep 0 --noise_char_random 0
-boudams dataset generate ../../tsv/oriflamms gt/*.txt  --max_char_length 150
+#convert("datasets/oriflamms_noallogr/*.txt", "datasets/oriflamms_noallogr/out", max_char_length=150, random_keep=0, noise_char_random=0)
+# generate("datasets/oriflamms_noallogr/split", "datasets/oriflamms_noallogr/out/train_DixFolios.txt", 150, 1, 0)
+# from boudams.dataset.base
+# split("datasets/oriflamms_noallogr/out/*.txt", "datasets/oriflamms_noallogr/split", (1, 0,0), 150)
+# check("./datasets/oriflamms_noallogr/split", (1,0,0), 150)
+##############
+#boudams dataset generate ../../tsv/oriflamms gt/*.txt  --max_char_length 150
 
