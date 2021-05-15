5f279d9da5cf10c8857ff5243777e1d2ebee0b26
Jean Batiste Camps <jbcamps@srv-rech01.enc.lan>
2021-05-15 14:11:01 +0200

    make sets

--- lat/tsv/make_sets.sh
+++ lat/tsv/make_sets.sh
@@ -3,7 +3,7 @@
 rm exper2_train.tsv
 rm exper3_train.tsv
 cat oriflamms/* btv1b9080806r_abbr/train.tsv >> exper2_train.tsv
-cat exper2_train.tsv PL/* >> exper3_train.tsv
+cat btv1b9080806r_expan/train.tsv PL/* >> exper3_train.tsv
 #cat */train.tsv */unknown.tsv > train.tsv
 #cat */dev.tsv > dev.tsv
 #cat */test.tsv > test.tsv
