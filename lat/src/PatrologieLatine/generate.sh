e15c914c417e5fa5e00adc9d098d05d0b10c71ae
Jean Batiste Camps <jbcamps@srv-rech01.enc.lan>
2022-04-08 18:43:33 +0200

    update generate

--- lat/src/PatrologieLatine/generate.sh
+++ lat/src/PatrologieLatine/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 boudams dataset convert plain-text ./gt ./PLnettoye/*.txt --max_char_length 150 --random_keep 0 --noise_char_random 0
-boudams dataset generate ../../tsv/PL gt/*.txt  --max_char_length 150
+boudams dataset generate ../../tsv/PL gt/*.txt  --max_char_length 150 --train 0.9 --test 0.05
 
